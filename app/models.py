5046b0cbcbb60814774d8bccaf401dd3af6753a3
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-03-30 19:45:26 -0700

    added login functionality, users must be logged in to see recipes

--- app/models.py
+++ app/models.py
@@ -10,15 +10,24 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 from datetime import datetime
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32))
-    password = db.Column(db.String(32))
-    email = db.Column(db.String(32))
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    def is_active(self):
+        return True
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,6 +36,7 @@ class Recipe(db.Model):
     ingredients = db.Column(db.Text, nullable = False)
     instructions = db.Column(db.Text, nullable = False)
     created = db.Column(db.DateTime, default=datetime.utcnow) # use current time
+    #user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
 
 
 
